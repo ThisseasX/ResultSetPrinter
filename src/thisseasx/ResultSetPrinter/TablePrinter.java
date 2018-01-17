c1a0d51feca2cc24f3396e1e6cdf46a8302d18eb
Thisseas X <thisseasx@yahoo.com>
2018-01-17 18:52:17 +0200

    Changed Package Name

--- src/thisseasx/ResultSetPrinter/TablePrinter.java
+++ src/thisseasx/ResultSetPrinter/TablePrinter.java
@@ -19,7 +19,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-package thisseasx;
+package thisseasx.ResultSetPrinter;
 
 import java.sql.ResultSet;
 import java.util.*;
